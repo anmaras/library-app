51c7e7c87c03cd5e4a790766ab73249e9a34324c
Antonis <marasantonis@gmail.com>
2022-03-28 21:16:01 +0300

    delete function wip update

--- script.js
+++ script.js
@@ -5,42 +5,33 @@ const bookSubmitBtn = document.querySelector(".button-submit");
 
 let myLibrary = [];
 
-const addBookHandler = function () {
-  backDrop.classList.add("visible");
-  libraryForm.classList.add("visible");
-};
-
-const addBackDropHandler = () => {
-  backDrop.classList.remove("visible");
-  libraryForm.classList.remove("visible");
-};
-
-function Books(title, author, pages) {
+function Books(title, author, pages, id) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
 }
 
-Books.prototype.upperCase = function () {
-  return `Book Title : ${this.title}`;
+Books.prototype.delete = function (e) {
+  // const list = document.querySelector(".books-list__items");
+  const listItem = document.querySelector(".book-list__item-title");
 };
 
 const renderBookList = () => {
   const booksList = document.querySelector(".books-list__items");
-  // if the size of array is not 0
   if (myLibrary.length !== 0) {
     booksList.classList.add("visible");
   }
 
-  booksList.replaceChildren();
+  booksList.innerHTML = "";
 
   myLibrary.forEach((book) => {
+    const list = document.createElement("li");
     const bookListTitle = document.createElement("div");
     const bookListAuthor = document.createElement("div");
     const bookListPage = document.createElement("div");
     const readStatusBtn = document.createElement("button");
     const deleteBookBtn = document.createElement("button");
-    const list = document.createElement("li");
 
     list.classList = "books-list__item";
     bookListTitle.className = "book-list__item-title";
@@ -49,7 +40,7 @@ const renderBookList = () => {
     readStatusBtn.className = "read-status";
     deleteBookBtn.className = "book-list__item-delete";
 
-    bookListTitle.textContent = book.upperCase();
+    bookListTitle.textContent = book.title;
     bookListAuthor.textContent = book.author;
     bookListPage.textContent = book.pages;
     readStatusBtn.textContent = "Read-Status";
@@ -61,22 +52,34 @@ const renderBookList = () => {
     list.append(bookListPage);
     list.append(readStatusBtn);
     list.append(deleteBookBtn);
+
+    deleteBookBtn.addEventListener("click", () => {
+      console.log(book.id);
+    });
   });
 };
-
-renderBookList();
 
 function addBookToLibraryHandler() {
   const bookTitle = document.getElementById("book-title").value;
   const bookAuthor = document.getElementById("book-author").value;
   const bookPages = document.getElementById("book-pages").value;
+  const randomId = Math.random();
 
-  const book = new Books(bookTitle, bookAuthor, bookPages);
+  const book = new Books(bookTitle, bookAuthor, bookPages, randomId);
   myLibrary.push(book);
-
   addBackDropHandler();
   renderBookList();
 }
+
+const addBookHandler = function () {
+  backDrop.classList.add("visible");
+  libraryForm.classList.add("visible");
+};
+
+const addBackDropHandler = () => {
+  backDrop.classList.remove("visible");
+  libraryForm.classList.remove("visible");
+};
 
 addBookBtn.addEventListener("click", addBookHandler);
 backDrop.addEventListener("click", addBackDropHandler);
