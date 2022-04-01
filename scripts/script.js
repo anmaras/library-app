31c15073229e10f7cd08a0bb5f25721c5298a0fd
Antonis <marasantonis@gmail.com>
2022-04-01 14:54:04 +0300

    add read status event and append fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-const addBookBtn = document.querySelector(".header-button");
+"use strict";
+const headerAddBookBtn = document.querySelector(".header-button");
 const backDrop = document.querySelector(".backdrop");
 const libraryForm = document.querySelector(".book-form");
 const bookSubmitBtn = document.querySelector(".button-submit");
@@ -6,30 +7,29 @@ const booksList = document.querySelector(".books-list__items");
 const HOT_MAGENTA = "#ff16cdff";
 const MELLOW_APROCOT = "#fdbb68ff";
 
+const mainFragment = new DocumentFragment();
+
 let myLibrary = [];
 
 // Utilities Functions
 
-const showBookFormHandler = function () {
-  backDrop.classList.add("visible");
-  libraryForm.classList.add("visible");
-  // formInputsClear();
+const formAndBackDropHandler = function () {
+  libraryForm.classList.toggle("visible");
+  backDrop.classList.toggle("visible");
+  formInputsValuesClear();
 };
 
-const formInputsClear = function () {
-  document.getElementById("book-title").value = "";
-  document.getElementById("book-author").value = "";
+const formInputsValuesClear = function () {
+  const formUserInputs = document.querySelectorAll("input");
+  for (let input of formUserInputs) {
+    if (input.type === "text") {
+      input.value = "";
+    }
+  }
 };
-
-const removeBackDropAndFormHandler = function () {
-  backDrop.classList.remove("visible");
-  libraryForm.classList.remove("visible");
-};
-
 const checkMyLibraryLength = () => {
   if (myLibrary.length !== 0) {
     booksList.classList.add("visible");
-    booksList.replaceChildren();
   } else return;
 };
 
@@ -43,109 +43,103 @@ function Books(title, author, readStatus) {
 
 //Library DOM create book items function
 
-const renderBookList = function () {
-  checkMyLibraryLength();
+Books.prototype.renderBookList = function (title, author, status) {
+  const list = document.createElement("li");
+  const listItemContainer = document.createElement("div");
+  const bookListTitle = document.createElement("div");
+  const starContainer = document.createElement("div");
+  const bookListAuthor = document.createElement("div");
+  const readStatusBtn = document.createElement("button");
+  const deleteIcon = document.createElement("img");
+  const starIcon = document.createElement("img");
+  const bookmarkCheckIcon = document.createElement("img");
+  const bookmarkMinusIcon = document.createElement("img");
 
-  myLibrary.forEach((book) => {
-    //Create  the li and li items
-    const list = document.createElement("li");
-    const listItemContainer = document.createElement("div");
-    const bookListTitle = document.createElement("div");
-    const starContainer = document.createElement("div");
-    const bookListAuthor = document.createElement("div");
-    const readStatusBtn = document.createElement("button");
-    const deleteIcon = document.createElement("img");
-    const starIcon = document.createElement("img");
-    const bookmarkCheckIcon = document.createElement("img");
-    const bookmarkMinusIcon = document.createElement("img");
+  deleteIcon.src = "/icons/trash-can-outline.svg";
+  starIcon.src = "/icons/star-outline.svg";
+  bookmarkCheckIcon.src = "/icons/bookmark-check.svg";
+  bookmarkMinusIcon.src = "/icons/bookmark-minus.svg";
 
-    //img srcs
-    deleteIcon.src = "/icons/trash-can-outline.svg";
-    starIcon.src = "/icons/star-outline.svg";
-    bookmarkCheckIcon.src = "/icons/bookmark-check.svg";
-    bookmarkMinusIcon.src = "/icons/bookmark-minus.svg";
+  list.className = "books-list__item";
+  listItemContainer.className = "book-list__item-container";
+  starContainer.className = "stars-container";
+  bookListTitle.className = "book-list__item-title";
+  bookListAuthor.className = "book-list__item-author";
+  readStatusBtn.className = "read-status";
+  deleteIcon.className = "book-list__item-delete";
+  starIcon.className = "rating-stars";
+  bookmarkCheckIcon.className = "bookmark-check";
+  bookmarkMinusIcon.className = "bookmark-minus";
 
-    //class names assign
-    list.className = "books-list__item";
-    listItemContainer.className = "book-list__item-container";
-    starContainer.className = "stars-container";
-    bookListTitle.className = "book-list__item-title";
-    bookListAuthor.className = "book-list__item-author";
-    readStatusBtn.className = "read-status";
-    deleteIcon.className = "book-list__item-delete";
-    starIcon.className = "rating-stars";
-    bookmarkCheckIcon.className = "bookmark-check";
-    bookmarkMinusIcon.className = "bookmark-minus";
+  bookListTitle.textContent = title;
+  bookListAuthor.textContent = `by ${author}`;
 
-    //text content assign
-    bookListTitle.textContent = `${book.title}`;
-    bookListAuthor.textContent = `by ${book.author}`;
+  if (status) {
+    readStatusBtn.style.background = HOT_MAGENTA;
+    readStatusBtn.append(bookmarkCheckIcon);
+  } else {
+    readStatusBtn.style.background = MELLOW_APROCOT;
+    readStatusBtn.append(bookmarkMinusIcon);
+  }
 
-    //append to list
-    list.append(listItemContainer);
+  for (let i = 0; i < 5; i++) {
+    starContainer.append(starIcon.cloneNode(true));
+  }
 
-    //append to listItemContainer
-    listItemContainer.append(
-      bookListTitle,
-      bookListAuthor,
-      readStatusBtn,
-      starContainer,
-      deleteIcon
-    );
-
-    //append star img 5 times
-    for (let i = 0; i < 5; i++) {
-      starContainer.append(starIcon.cloneNode(true));
-    }
-
-    if (book.readStatus) {
-      readStatusBtn.id = "read";
-      readStatusBtn.style.background = HOT_MAGENTA;
-      readStatusBtn.append(bookmarkCheckIcon);
-    } else {
-      readStatusBtn.id = "not_read";
-
+  readStatusBtn.addEventListener("click", () => {
+    if (status) {
+      status = false;
       readStatusBtn.style.background = MELLOW_APROCOT;
-      readStatusBtn.append(bookmarkMinusIcon);
+      bookmarkCheckIcon.replaceWith(bookmarkMinusIcon);
+    } else {
+      status = true;
+      readStatusBtn.style.background = HOT_MAGENTA;
+      bookmarkMinusIcon.replaceWith(bookmarkCheckIcon);
     }
-
-    deleteIcon.addEventListener("click", function () {
-      booksList.removeChild(this.parentElement.parentElement);
-      myLibrary.splice(this, 1);
-    });
-
-    readStatusBtn.addEventListener("click", () => {
-      if (readStatusBtn.id === "read") {
-        readStatusBtn.id = "not_read";
-        bookmarkCheckIcon.replaceWith(bookmarkMinusIcon);
-        readStatusBtn.style.background = MELLOW_APROCOT;
-      } else if (readStatusBtn.id === "not_read") {
-        readStatusBtn.id = "read";
-        bookmarkMinusIcon.replaceWith(bookmarkCheckIcon);
-        readStatusBtn.style.background = HOT_MAGENTA;
-      }
-    });
-    booksList.append(list);
   });
+
+  deleteIcon.addEventListener("click", function () {
+    booksList.removeChild(this.parentElement.parentElement);
+    myLibrary.splice(this, 1);
+  });
+
+  listItemContainer.append(
+    bookListTitle,
+    bookListAuthor,
+    readStatusBtn,
+    starContainer,
+    deleteIcon
+  );
+  list.append(listItemContainer);
+  mainFragment.append(list);
 };
+
+Books.prototype.append = function () {
+  booksList.append(mainFragment);
+};
+
+window.addEventListener("click", (e) => {
+  console.log(e.target);
+});
 
 function addBookToLibraryHandler() {
   const bookTitle = document.getElementById("book-title").value;
   const bookAuthor = document.getElementById("book-author").value;
   const checkbox = document.getElementById("book-read").checked;
 
-  // if (bookTitle === "" || bookAuthor === "" || bookPages === "") return;
+  if (bookTitle.trim() === "" || bookAuthor.trim() === "") return;
 
   const book = new Books(bookTitle, bookAuthor, checkbox);
 
-  myLibrary.push(book);
-
-  renderBookList();
-  removeBackDropAndFormHandler();
+  myLibrary.splice(0, 0, book);
+  checkMyLibraryLength();
+  book.renderBookList(book.title, book.author, checkbox);
+  book.append();
+  formAndBackDropHandler();
 }
 
-addBookBtn.addEventListener("click", showBookFormHandler);
+headerAddBookBtn.addEventListener("click", formAndBackDropHandler);
 
-backDrop.addEventListener("click", removeBackDropAndFormHandler);
+backDrop.addEventListener("click", formAndBackDropHandler);
 
 bookSubmitBtn.addEventListener("click", addBookToLibraryHandler);
